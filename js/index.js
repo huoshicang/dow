af3ff4687797e6531e11ef659b27026d82a8daa8
huoshicang <74391377+huoshicang@users.noreply.github.com>
2021-08-24 19:42:06 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+    var documentation = $('.documentation');
+    console.log("文档是自己整理的，但不一定会，如果有错误的地方可以发邮件联系：2717334980@qq.com");
+    documentation.each(function(indexInArray, valueOfElement) {
+        var href;
+        $(valueOfElement).click(function(e) {
+            href = valueOfElement.title
+            $('.content').html(`<object type="text/html" data=${href} width="100%" height="100%"></object>`);
+            var file_url = href.replace(/html/g, 'md');
+            $('.nav a').attr('href', file_url);
+        });
+    });
+
+});
---
+++ js/issue.js
@@ -0,0 +1,30 @@
+$(function() {
+    $.each($('.panel'), function(indexInArray, item) {
+        item.addEventListener('click', () => {
+            $.each($('.panel'), function(indexInArray, a) {
+                a.classList.remove('active');
+            });
+            item.classList.add('active');
+        });
+
+        //    
+    });;
+    // $('.panel').click(function(e) {
+    //     console.log('ss');
+    // });
+    // $('.panmel').each(function(index, element) {
+    //     element.click(function(e) {
+    //         console.log('ss');
+    //     })
+    // });
+});
+
+// const panels = document.querySelectorAll('.panel');
+// panels.forEach((item) => {
+//     item.addEventListener('click', () => {
+//         panels.forEach((e) => {
+//             e.classList.remove('active');
+//         });
+//         item.classList.add('active');
+//     });
+// });
